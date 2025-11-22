2b157ea990dc3c60fa3295af2b42d670f6e93063
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-21 21:14:52 -0300

    configuro bd

--- gestion_usuarios/settings.py
+++ gestion_usuarios/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'gestion_usuarios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gestion_usuarios_django_mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
